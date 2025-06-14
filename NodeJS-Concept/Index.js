bfb224f3f9c825e4f29fa847b9c747e9ad882ad3
salma abusier <salma4567omer@gmail.com>
2025-06-14 15:02:01 +0300

    node

---
+++ NodeJS-Concept/Index.js
@@ -0,0 +1,9 @@
+/* require() to call package */
+var colors = require('colors');
+
+/* the way we can use the package */ 
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.random); // Drops the bass
